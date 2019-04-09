3fc8039124ea373b7dedfd1fbb56a15c64849ffc
Alexanderdunlop <alex.dunlop@orbba.com>
2019-04-09 17:09:46 +1200

    Add prod function

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,9 @@
-const sum = require('./sum');
+const { sum, prod } = require('./sum');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('prod calculates 2 * 10 to equal 20', () => {
+  expect(prod(2, 10)).toBe(20);
 });
